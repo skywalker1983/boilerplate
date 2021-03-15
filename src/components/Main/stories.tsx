6993591ec32456bdd4e5199c1a6a3ae23b336b03
skywalker1983 <ljunqueira@gmail.com>
2021-03-14 21:01:58 -0300

    Upload completo do boilerplate

---
+++ src/components/Main/stories.tsx
@@ -0,0 +1,14 @@
+import { Story, Meta } from '@storybook/react'
+import Main from '.'
+
+export default {
+  title: 'Main',
+  component: Main,
+  args: {
+    title: 'title default',
+    description: 'description default',
+  },
+} as Meta
+
+export const Basic: Story = (args) => <Main {...args} />
+export const Default: Story = (args) => <Main {...args} />
